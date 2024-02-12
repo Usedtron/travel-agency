baed0b7ade1738523e1daccb7fa703d567bac62e
Nicolai <usedtron@gmail.com>
2024-02-12 11:06:26 +0000

    update footer more responsive

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -4,100 +4,91 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-200 text-gray-800 p-10 ">
-      <div className="container mx-auto justify-center space-x-3  flex flex-row">
-        <Link href="https://www.livroreclamacoes.pt/Inicio/" target="_blank">
-          <p>
-            <Image
-              src="/reclamacao.png"
-              alt="Livro de reclamações"
-              width={80}
-              height={80}
-            />
-          </p>
-        </Link>
-        <Link
-          href="http://www.turismodeportugal.pt/pt/Paginas/homepage.aspx"
-          target="_blank"
-        >
-          <p>
-            <Image
-              src="/turismo.png"
-              alt="Turismo de Portugal"
-              width={80}
-              height={80}
-            />
-          </p>
-        </Link>
-        <Link
-          href="https://www.instagram.com/paraaquelesqueamamviajar/"
-          target="_blank"
-        >
-          <p>
-            <Image
-              src="/instagram.svg"
-              alt="Turismo de Portugal"
-              width={30}
-              height={30}
-            />
-          </p>
-        </Link>
-        <Link
-          href="https://www.facebook.com/profile.php?id=100072803705563"
-          target="_blank"
-        >
-          <p>
-            <Image
-              src="/facebook.svg"
-              alt="Turismo de Portugal"
-              width={30}
-              height={30}
-            />
-          </p>
-        </Link>
-       <Link
-       target="_blank"
-       href="https://wa.me/351966420212?text=Ola%20Para Aqueles que Amam Viajar,%20tenho%20uma%20consulta%20..."
-       >
-        <p>
+<footer className="bg-gray-200 text-gray-800 p-4 md:p-10">
+  <div className="container mx-auto flex flex-col md:flex-row items-center justify-center md:justify-between space-y-4 md:space-y-0 md:space-x-3">
+    <div className="flex space-x-3">
+      <Link href="https://www.livroreclamacoes.pt/Inicio/" target="_blank">
         <Image
-        src="/whatsapp.svg"
-        alt="Turismo de Portugal"
-        width={30}
-        height={30}
+          src="/reclamacao.png"
+          alt="Livro de reclamações"
+          width={80}
+          height={80}
         />
-        </p>
+      </Link>
+      <Link
+        href="http://www.turismodeportugal.pt/pt/Paginas/homepage.aspx"
+        target="_blank"
+      >
+        <Image
+          src="/turismo.png"
+          alt="Turismo de Portugal"
+          width={80}
+          height={80}
+        />
+      </Link>
+      <Link
+        href="https://www.instagram.com/paraaquelesqueamamviajar/"
+        target="_blank"
+      >
+        <Image
+          src="/instagram.svg"
+          alt="Turismo de Portugal"
+          width={30}
+          height={30}
+        />
+      </Link>
+      <Link
+        href="https://www.facebook.com/profile.php?id=100072803705563"
+        target="_blank"
+      >
+        <Image
+          src="/facebook.svg"
+          alt="Turismo de Portugal"
+          width={30}
+          height={30}
+        />
+      </Link>
+      <Link
+        target="_blank"
+        href="https://wa.me/351966420212?text=Ola%20Para Aqueles que Amam Viajar,%20tenho%20uma%20consulta%20..."
+      >
+        <Image
+          src="/whatsapp.svg"
+          alt="Turismo de Portugal"
+          width={30}
+          height={30}
+        />
+      </Link>
+    </div>
+    <div className="text-xs flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+      <p>
+        &copy; {new Date().getFullYear()} Para Aqueles que Amam Viajar |
+        RNAVT n.º 11.332
+        <br />
+        Rua do Movimento Republicano, 16, 1.º Esquerdo Parque Luso 2855-716
+        Corroios
+      </p>
+      <div className="flex space-x-4">
+        <Link href="/legal-notice">
+          <p>Aviso Legal</p>
+        </Link>
+        <Link href="/conditions">
+          <p>Condições Gerais</p>
+        </Link>
+        <Link href="/cookies">
+          <p>Política de Cookies</p>
+        </Link>
+        <Link href="/privacy-policy">
+          <p>Política de Privacidade</p>
+        </Link>
+        <Link href="/contacts">
+          <p>Contatos</p>
         </Link>
       </div>
-      <div className="container mx-auto">
-        <div className="flex justify-between items-center">
-          <p className="text-xs">
-            &copy; {new Date().getFullYear()} Para Aqueles que Amam Viajar |
-            RNAVT n.º 11.332
-            <br />
-            Rua do Movimento Republicano, 16, 1.º Esquerdo Parque Luso 2855-716
-            Corroios
-          </p>
-          <div className="flex space-x-4">
-            <Link href="/legal-notice">
-              <p className="text-xs">Aviso Legal</p>
-            </Link>
-            <Link href="/conditions">
-              <p className="text-xs">Condições Gerais</p>
-            </Link>
-            <Link href="/cookies">
-              <p className="text-xs">Política de Cookies</p>
-            </Link>
-            <Link href="/privacy-policy">
-              <p className="text-xs">Política de Privacidade</p>
-            </Link>
-            <Link href="/contacts">
-              <p className="text-xs">Contatos</p>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </footer>
+    </div>
+  </div>
+</footer>
+
   );
 };
 
