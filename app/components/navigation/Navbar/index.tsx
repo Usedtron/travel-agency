3f4cf42ee7d000e40ebc0b33f703a110badce895
Nicolai <usedtron@gmail.com>
2024-02-13 15:54:36 +0000

    implement svg navbar

--- app/components/navigation/Navbar/index.tsx
+++ app/components/navigation/Navbar/index.tsx
@@ -10,7 +10,7 @@ type Props = {
 const Navbar = (props: Props) => {
   return (
     <div className="navbar bg-white text-orange-400 py-2 fixed w-full top-0 z-10 shadow-md h-6">
-      <div className="navbar-start">
+      <div className="w-3/6 flex justify-between ">
         <div className="flex items-center">
           <Image
             className="logoimage"
@@ -19,17 +19,38 @@ const Navbar = (props: Props) => {
             width={150}
             height={150}
           />
-          <a href="/#home" className="btn btn-ghost text-sm md:text-xl font-bold">
+          <a
+            href="/#home"
+            className="btn btn-ghost text-sm md:text-xl font-bold"
+          >
             PARA AQUELES QUE AMAM VIAJAR
           </a>
+          <div className="lg:hidden navbar-end">
+            <button className="btn btn-square btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-5 h-5 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </button>
+          </div>
         </div>
       </div>
-    <div className="navbar-center hidden lg:flex">
+
+      <div className="hidden navbar-center lg:flex">
         <a href="/form" className="btn bg-orange-300 text-white btn-sm">
           Enviar Pedido
         </a>
       </div>
-      <div className="navbar-end hidden lg:flex">
+      <div className="hidden navbar-end lg:flex">
         <ul className="menu menu-horizontal px-1 text-gray-600">
           <li>
             <a href="/#destinos" className="hover:text-orange-500">
