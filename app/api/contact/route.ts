7b8e5cb9e7a6df0f9b682f12ec2d5f086e6b2e60
Nicolai <usedtron@gmail.com>
2024-03-11 01:06:41 +0000

    email title Pedido de contacto ajustado

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -22,7 +22,7 @@ export async function POST(request: NextRequest, response: NextResponse<Response
     const data = await resend.emails.send({
       from: "Cliente <noreply@paraaquelesqueamamviajar.pt>", // will change after domain verify
       to: ["usedtron@gmail.com"], // client's email
-      subject: "Pedido de orçamento",
+      subject: "Pedido de Contacto",
       react: EmailTemplate({ 
         name: name,
         email: email,
