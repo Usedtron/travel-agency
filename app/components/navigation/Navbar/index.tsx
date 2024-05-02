558056d5fa30a7db2490ea87613a172be5f6df12
Nicolai <usedtron@gmail.com>
2024-05-02 12:42:53 +0100

    update central buttton

--- app/components/navigation/Navbar/index.tsx
+++ app/components/navigation/Navbar/index.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   };
   return (
     <div className="navbar bg-white text-orange-500 py-2 fixed w-full top-0 z-10 shadow-md h-6">
-      <div className="navbar-start">
+      <div className="navbar-start ">
         <div className="flex items-center">
           <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
           <Image
@@ -28,9 +28,9 @@ const Navbar = () => {
           </a>
         </div>
       </div>
-      <div className="navbar-center hidden lg:flex">
+      <div className="navbar-center hidden lg:flex w-1/6">
         <details className="dropdown ">
-          <summary className="btn bg-orange-400 text-white border-white btn-sm flex rounded-none w-54 p-2">RESERVAR</summary>
+          <summary className="btn bg-orange-400 text-white border-white btn-MD flex rounded xl:w-36 lg:w-36 p-2 lx:font-bold">RESERVAR ONLINE</summary>
           <ul className="p-2 shadow menu-vertical dropdown-content z-[1] bg-base-100 ">
             <li>
               {" "}
@@ -62,15 +62,15 @@ const Navbar = () => {
           </ul>
         </details>
       </div>
-      <div className="navbar-end hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 text-gray-600">
+      <div className="navbar-end hidden lg:flex w-2/5">
+        <ul className="menu menu-horizontal  text-gray-600">
           <li>
             <a href="/#destinos" className="hover:text-orange-500">
               Destinos
             </a>
           </li>
           <li>
-            <a href="/#quemsomos" className="hover:text-orange-500">
+            <a href="/#quemsomos" className="hover:text-orange-500 ">
               Quem Somos
             </a>
           </li>
